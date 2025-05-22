4dfedb2d2c667556a512473ac9a2640e8af3c20f
Balaji0502 <161752065+Balaji0502@users.noreply.github.com>
2025-05-22 23:04:14 +0530

    Create InterfaceImplementation.java

---
+++ corejava/InterfaceImplementation.java
@@ -0,0 +1,23 @@
+interface Playable {
+    void play();
+}
+class Guitar implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Playing the guitar 🎸");
+    }
+}
+class Piano implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Playing the piano 🎹");
+    }
+}
+public class InterfaceImplementation {
+    public static void main(String[] args) {
+        Playable myGuitar = new Guitar();
+        Playable myPiano = new Piano();
+        myGuitar.play();
+        myPiano.play();
+    }
+}
